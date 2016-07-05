7ef6d76a1f944d028a8cb7c19ba5a91722d1840a
Baptiste Amato <baptiste.amato@gmail.com>
2016-07-06 00:51:19 +0200

    Create EnemyStanding animation

---
+++ TX VR/Assets/Script/Enemy/testsAnimations.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testsAnimations : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("toto");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    IEnumerator toto()
+    {
+        yield return new WaitForSeconds(2);
+        gameObject.GetComponent<Animator>().Play("EnemyStanding");
+    }
+
+}
