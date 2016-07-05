7ba072e080d0a8ec16459a8b783cc2274691c028
Terisse Nicolas <nicolas.terisse@gmail.com>
2016-07-05 18:21:19 +0200

    Scene decoration update

---
+++ TX VR/Assets/Script/Game/Game.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Game : MonoBehaviour {
+
+    List<Enemy> enemyList;
+    int enemyCountInit = 6;
+    int enemyRemaining;
+
+    void Awake()
+    {
+        this.enemyRemaining = this.enemyCountInit;
+    }
+
+	void Start ()
+    {
+        int i;
+        for (i=1; i<= enemyCountInit; i++)
+        {
+            enemyList.Add(GameObject.Find("CowBoy" + i.ToString()).GetComponent<Enemy>());
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
