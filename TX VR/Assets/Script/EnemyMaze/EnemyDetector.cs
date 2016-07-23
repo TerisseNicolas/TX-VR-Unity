dd5db3c1adf47be5ede470a9a25ded578b73a905
Terisse Nicolas <nicolas.terisse@gmail.com>
2016-07-23 22:00:42 +0200

    Enemy in maze scene

---
+++ TX VR/Assets/Script/EnemyMaze/EnemyDetector.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyDetector : MonoBehaviour
+{
+    public bool targetDetected = false;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if ((col.gameObject.transform.root.name == "target") && (targetDetected == false))
+        {
+            this.targetDetected = true;
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if ((col.gameObject.transform.root.name == "target") && (targetDetected == true))
+        {
+            this.targetDetected = false;
+        }
+    }
+}
