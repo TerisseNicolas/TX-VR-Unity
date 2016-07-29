1281e7a7d5503a5d7eb58fca0e5c3e8989840658
Baptiste Amato <baptiste.amato@gmail.com>
2016-07-29 19:29:55 +0200

    After VR test

--- TX VR/Assets/Script/VR/WandController.cs
+++ TX VR/Assets/Script/VR/WandController.cs
@@ -51,6 +51,7 @@ public class WandController : MonoBehaviour {
         if (triggerButtonDown)
         {
             Debug.Log("TriggerDown was just pressed");
+			GetComponentInChildren<BulletContainer> ().shot ();
         }
         if (triggerButtonUp)
         {
