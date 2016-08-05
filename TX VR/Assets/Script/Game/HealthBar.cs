6508e596c00e8694ab256d307548e3a9dedba261
Terisse Nicolas <nicolas.terisse@gmail.com>
2016-08-05 21:16:51 +0200

    Health bar

---
+++ TX VR/Assets/Script/Game/HealthBar.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    Slider slider;
+
+    void Start()
+    {
+        this.slider = gameObject.GetComponent<Slider>();
+    }
+
+    public void updateValue(float newLife)
+    {
+        this.slider.value = newLife / 100;
+    }
+}
