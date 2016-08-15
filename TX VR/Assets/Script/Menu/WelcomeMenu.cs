17d1944fb332a71f87680973a355966eb7c3cfbe
Terisse Nicolas <nicolas.terisse@gmail.com>
2016-08-15 19:20:45 +0200

    New menu scene

---
+++ TX VR/Assets/Script/Menu/WelcomeMenu.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WelcomeMenu : MonoBehaviour
+{
+    private Button buttonShooter;
+    private Button buttonScores;
+    private Button buttonQuit;
+
+	void Start ()
+    {
+        buttonShooter = GameObject.Find("ButtonShooter").GetComponent<Button>();
+        buttonScores = GameObject.Find("ButtonScores").GetComponent<Button>();
+        buttonQuit = GameObject.Find("ButtonQuit").GetComponent<Button>();
+
+        buttonShooter.onClick.AddListener(buttonShooterClicked);
+        buttonScores.onClick.AddListener(() => { buttonScoresClicked(); });
+        buttonQuit.onClick.AddListener(() => { buttonQuitClicked(); });
+    }
+
+    void buttonShooterClicked()
+    {
+        Debug.Log("clicked");
+        SceneSupervisor.loadShooterScene();
+    }
+
+    void buttonScoresClicked()
+    {
+        SceneSupervisor.loadScoresScene();
+    }
+
+    void buttonQuitClicked()
+    {
+        Application.Quit();
+    }
+}
