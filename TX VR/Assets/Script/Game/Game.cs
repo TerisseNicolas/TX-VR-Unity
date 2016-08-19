a9f37f4163713ad631217fde979e1ed1f9aaf2e2
Terisse Nicolas <nicolas.terisse@gmail.com>
2016-08-19 22:20:46 +0200

    Timer

--- TX VR/Assets/Script/Game/Game.cs
+++ TX VR/Assets/Script/Game/Game.cs
@@ -1,15 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class Game : MonoBehaviour
 {
     EnemyManager enemyManager;
     GameObject target;
-    public bool gameEnd = false;
+    public int gameEnd = 0;
+    public int playTime = 180;
+    public int remainingPlayTime;
+
+    private Text timerText;
 
     void Start()
     {
+        this.remainingPlayTime = this.playTime;
+        this.timerText = GameObject.Find("Timer").GetComponent<Text>();
+        this.timerText.text = this.remainingPlayTime.ToString();
         this.enemyManager = gameObject.GetComponent<EnemyManager>();
         this.target = GameObject.Find("target");
         this.target.GetComponent<LifeManager>().DeathEvent += target_DeathEvent;
@@ -20,6 +28,7 @@ public class Game : MonoBehaviour
     IEnumerator startGame()
     {
         yield return new WaitForSeconds(3f);
+        StartCoroutine(gameTimer());
         Debug.Log("Starting game ===========================");
         StartCoroutine(this.enemyManager.startGame());
         yield return StartCoroutine(endOfGameCheck());
@@ -33,10 +42,27 @@ public class Game : MonoBehaviour
         {
             yield return new WaitForSeconds(1f);
         }
+        endOfGame();
+    }
 
-        //End of game Todo
-        Debug.Log("End of game============================");
-        this.gameEnd = true;
+    IEnumerator gameTimer()
+    {
+        for(int i=this.playTime; i>0; i--)
+        {
+            yield return new WaitForSeconds(1f);
+            this.remainingPlayTime--;
+            this.timerText.text = this.remainingPlayTime.ToString();
+        }
+        endOfGame();
+    }
+
+    private void endOfGame()
+    {
+        this.gameEnd++;
+        if(this.gameEnd == 1)
+        {
+            //todo
+        }
     }
 
     //When the player died
